67eb8ca0b63bc5bcc79753bcd251b0bee0da4200
Cybinon <nikorunikk@gmail.com>
2023-01-15 21:57:43 +0800

    three js added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export default function Navbar() {
+  return (
+    <Box minH={"100vh"} background="https://cdn.dribbble.com/users/1171505/screenshots/15935246/media/c5c3b336ce8fb771f9c8d544b8d689e8.png">
+      
+    </Box>
+  )
+}
