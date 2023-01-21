f8fd8c3f2fbcb62be8f3e007c2e292fc9229a79e
Cybinon <nikorunikk@gmail.com>
2023-01-21 12:48:07 +0800

    connecting backend step

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,41 @@
 import { Container, Center, Grid } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import Card from '../src/components/Other/Card'
 import Hero from '../src/components/Other/Hero'
+import axios from "axios"
 
 export default function Home() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://cubezet-hfnf.vercel.app/product',
+    }).then(({data}) => {
+      setProducts(data.map((product:any) => {
+        return {
+          id: product.id,
+          name: product.name,
+          price: product.price,
+          image: product.image,
+          description: product.description,
+          salePrice: product.salePrice,
+          sold: product.sold,
+        }
+      }));
+    })
+
+  }, [])
   return (
     <>
       <Container mt="10px" minW="100%">
         <Hero />
         <Center>
           <Grid w="100%" templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]} gap={6}>
-            {data.map((item) => <Card w={"100%"} key={item.id} data={item} />)}
+            {products.map((item:any) => <Card w={"100%"} key={item.id} data={item} />)}
           </Grid>
         </Center>
       </Container>
     </>
   )
 }
-
-const data = [
-  {
-    id: "1",
-    title: "CUBEZET",
-    image: "https://cdn.midjourney.com/7f800228-9afc-4016-b31d-519c17942a8b/grid_0.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem ",
-    price: 3200,
-    sold: 1000,
-    salePrice: 1000,
-    trigger: () => alert("Added to cart"),
-  },
-  {
-    id: "2",
-    title: "CUBEZET",
-    image: "https://cdn.midjourney.com/7f800228-9afc-4016-b31d-519c17942a8b/grid_0.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem ",
-    price: 3200,
-    salePrice: 1000,
-    trigger: () => alert("Added to cart"),
-  },
-  {
-    id: "3",
-    title: "CUBEZET",
-    image: "https://cdn.midjourney.com/7f800228-9afc-4016-b31d-519c17942a8b/grid_0.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem m quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eget aliquam nisl lorem quis nisl. lorem ",
-    price: 3200,
-    salePrice: 1000,
-    trigger: () => alert("Added to cart"),
-  },
-]
