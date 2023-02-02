fec2c7dd9cabe743ca3bcf051ce0b0c50b379e63
Cybinon <nikorunikk@gmail.com>
2023-02-03 02:02:45 +0800

    added key

--- src/components/Game/Rooms.tsx
+++ src/components/Game/Rooms.tsx
@@ -1,9 +1,9 @@
 import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
 
-export default function Rooms({data}: any) {
+export default function Rooms({ data }: any) {
   return (
     <TableContainer p={3} border="1px solid teal" borderRadius={"20px"}>
-      <Table size="md" overflow="auto"  h={"400px"}>
+      <Table size="md" overflow="auto" h={"400px"}>
         <TableCaption>Earn big, Play smart</TableCaption>
         <Thead>
           <Tr>
@@ -14,13 +14,13 @@ export default function Rooms({data}: any) {
           </Tr>
         </Thead>
         <Tbody>
-          {data.map((room: any) => (
-            <Tr p={3} cursor="pointer">
-            <Td maxW={["90px","200px"]} maxH="60px" overflow={"hidden"}> jasldjalk jklasjdlkajsld jalkjd lakjl dkjalskdj lasjdlajsldjowqijhp oqh uwqh uh</Td>
-            <Td>8$</Td>
-            <Td display={["none", "block"]}>address</Td>
-            <Td>test</Td>
-          </Tr>
+          {data.map((room: any, i:number) => (
+            <Tr p={3} cursor="pointer" key={room+i}>
+              <Td maxW={["90px", "200px"]} maxH="60px" overflow={"hidden"}> </Td>
+              <Td>8$</Td>
+              <Td display={["none", "block"]}>address</Td>
+              <Td>test</Td>
+            </Tr>
           ))}
         </Tbody>
       </Table>
