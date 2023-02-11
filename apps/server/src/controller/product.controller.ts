4352c793f9056c67f39fba34bb785283123a822c
Cybinon <nikorunikk@gmail.com>
2023-02-12 01:00:26 +0800

    file link change

--- apps/server/src/controller/product.controller.ts
+++ apps/server/src/controller/product.controller.ts
@@ -47,7 +47,7 @@ export class ProductController {
           id: '3',
           title: 'Zero 2 Figure ',
           image:
-            'https://cdn.shopify.com/s/files/1/0014/2648/9388/products/ultra-tokyo-connection-pvc-scale-figures-chainsaw-man-power-prize-figure-32504714756140_360x.jpg?v=1669223937',
+            'https://i.ibb.co/7jTTMXh/82d205934a4ecbf9d935d1c8e07fa90f.webp',
           brand: 'Brand 1',
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl nisl sit amet lorem.',
