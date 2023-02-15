0289b8c62abb8f0feb5cf074151ea709e9ca44b1
Cybinon <nikorunikk@gmail.com>
2023-02-15 23:26:10 +0800

    hexason done

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -7,7 +7,7 @@ import {
   useColorModeValue,
   VisuallyHidden,
 } from '@chakra-ui/react';
-import { FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { ReactNode } from 'react';
 import Link from 'next/link';
 
@@ -63,8 +63,8 @@ export default function Footer() {
           <Text>© {new Date().getFullYear()}  Та яг ийм сайт хийлгэмээр байна уу? <Text as="span" color="blue.400"><Link target={"_blank"} href={"https://www.facebook.com/profile.php?id=100090518070093"}>
             ЭНД ДАРЖ</Link></Text> холбогдоорой</Text>
           <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Instagram'} href={'#'}>
-              <FaInstagram />
+            <SocialButton label={'Facebook'} href={'https://www.facebook.com/profile.php?id=100090518070093'}>
+              <FaFacebook />
             </SocialButton>
           </Stack>
         </Container>
