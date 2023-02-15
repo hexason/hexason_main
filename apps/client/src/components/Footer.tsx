1d893283680a38f2463be90c79a08d6467501263
Cybinon <nikorunikk@gmail.com>
2023-02-15 23:23:15 +0800

    Footer done

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -9,6 +9,7 @@ import {
 } from '@chakra-ui/react';
 import { FaInstagram } from 'react-icons/fa';
 import { ReactNode } from 'react';
+import Link from 'next/link';
 
 
 const SocialButton = ({
@@ -59,7 +60,8 @@ export default function Footer() {
           spacing={4}
           justify={{ base: 'center', md: 'space-between' }}
           align={{ base: 'center', md: 'center' }}>
-          <Text>© {new Date().getFullYear()}  All rights reserved</Text>
+          <Text>© {new Date().getFullYear()}  Та яг ийм сайт хийлгэмээр байна уу? <Text as="span" color="blue.400"><Link target={"_blank"} href={"https://www.facebook.com/profile.php?id=100090518070093"}>
+            ЭНД ДАРЖ</Link></Text> холбогдоорой</Text>
           <Stack direction={'row'} spacing={6}>
             <SocialButton label={'Instagram'} href={'#'}>
               <FaInstagram />
