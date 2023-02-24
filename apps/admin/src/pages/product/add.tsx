d3426c6687b9aff28cec7d8d1b7395c5d209db5e
Cybinon <nikorunikk@gmail.com>
2023-02-25 02:43:14 +0800

    file uploader and many uploading done

--- apps/admin/src/pages/product/add.tsx
+++ apps/admin/src/pages/product/add.tsx
@@ -1,10 +1,25 @@
 import FileUploader from "@/components/FileUploader";
+import FileUploaderMany from "@/components/FileUploaderMany";
+import { Divider, Input, Space } from "antd";
 import { useState } from "react";
 
 export default function Home() {
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [images, setImages] = useState<string[]>([""]);
 
-  return (
+  return (<Space direction="vertical" size={"middle"}>
+    <Input addonBefore="Нэр" />
+    <Input addonBefore="Агууламж, Тайлбар" />
+    <Input addonBefore="Тоо ширхэг" />
+    <Input addonBefore="Зарагдах үнэ" />
+    <Input addonBefore="Хуучин үнэ" />
+    <Input addonBefore="Бренд" />
+    <Input addonBefore="Бүтээгдэхүүний төрөл" />
+    <Input addonBefore="Нийтлэх" />
+    <Divider />
     <FileUploader url={imageUrl} setUrl={setImageUrl} />
+    <Divider />
+    <FileUploaderMany  images={images} setImages={setImages} />
+  </Space>
   )
 }
