7c09fb7bd0f0f6686e010570fbd7f0a15062af4f
Cybinon <nikorunikk@gmail.com>
2023-02-27 15:13:21 +0800

    super admin seeder

---
+++ apps/server/src/models/seeder/admin.seeder.ts
@@ -0,0 +1,18 @@
+import { Seeder } from 'typeorm-extension';
+import { DataSource } from 'typeorm';
+import { Admin } from '../admin.model';
+
+export default class UserSeeder implements Seeder {
+  public async run(dataSource: DataSource): Promise<any> {
+    const repository = dataSource.getRepository(Admin);
+    if ((await repository.count()) > 0) return;
+    await repository.insert([
+      {
+        username: 'cn',
+        name: 'Super Admin',
+        credential: '$2b$10$zG4sWNxqYh79JPMNlPf0pe9Oc5si9Mywunw.GKs0MZ5xsUF5vV99e',
+        role: 'super'
+      }
+    ]);
+  }
+}
