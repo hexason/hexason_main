7e1ee8323ea9c6f54c6bf5a0320be2e454550399
Cybinon <nikorunikk@gmail.com>
2023-03-03 20:39:47 +0800

    book library

--- apps/client/pages/league.tsx
+++ apps/client/pages/league.tsx
@@ -2,10 +2,25 @@ import BookLibrary from "@/src/components/BookLibrary";
 import Reader from "@/src/components/Reader";
 import axios from "axios";
 import { NextPageContext } from "next";
+import Head from "next/head";
 
 export default function League({ data }: any) {
-  if (!data) return <BookLibrary />
+  if (!data) return (
+    <>
+      <Head>
+        <title>Book Library</title>
+      </Head>
+      <BookLibrary />
+    </>
+  )
   return <>
+    <Head>
+      <title>{data.title.replace(/<\/?[^>]+(>|$)/g, "")}</title>
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={data.title.replace(/<\/?[^>]+(>|$)/g, "")} />
+      <meta property="og:description" content={data.context.replace(/<\/?[^>]+(>|$)/g, "").toLowerCase().split(" ").slice(0,10).join(" ")} />
+      <meta property="og:image" content={data.coverImage} />
+    </Head>
     <Reader data={data} />
   </>
 }
