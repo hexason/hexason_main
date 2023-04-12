3ea97e7669613c15c2eaaddb3e852d12101dd384
Cybinon <nikorunikk@gmail.com>
2023-04-12 17:05:11 +0800

    not useful change

--- apps/server/src/service/admin.service.ts
+++ apps/server/src/service/admin.service.ts
@@ -5,7 +5,7 @@ import { DataSource, Repository } from "typeorm";
 export class AdminService {
   adminRepo: Repository<Admin>
   constructor(@InjectDataSource() private readonly dataSource: DataSource) { 
-    this.adminRepo = dataSource.getRepository(Admin);
+    this.adminRepo = this.dataSource.getRepository(Admin);
   }
 
   async getAdminByEmail({email}) {
