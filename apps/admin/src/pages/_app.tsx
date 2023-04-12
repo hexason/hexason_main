40933e4032981d1baf618d0c33d3464db695933d
Cybinon <nikorunikk@gmail.com>
2023-04-13 00:27:25 +0800

    lock updated

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import NextNProgress from "nextjs-progressbar"
 import { ChakraProvider } from '@chakra-ui/react';
-import { AuthContext, AuthContextProvider } from '@/context/AuthContext';
+import { AuthContextProvider } from '@/context/AuthContext';
 
 
 export default function App({ Component, pageProps }: AppProps) {
