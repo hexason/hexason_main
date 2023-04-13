3f9c45832462061e39429647176cbbcbe4326a3b
Cybinon <nikorunikk@gmail.com>
2023-04-14 05:31:12 +0800

    added path

--- apps/admin/src/pages/api/pages.ts
+++ apps/admin/src/pages/api/pages.ts
@@ -1,18 +1,24 @@
-import { readdirSync } from "fs";
 import { NextRequest } from "next/server";
 
 export default function handler(req: NextRequest, res: any) {
-  const dirList = readdirSync(__dirname + "/../page");
 
-  const buttons = dirList.map(page => {
-    const represnted = page.replace(/\.ts$|\.js$/g, "");
-    return { url: "/page/" + represnted, txt: represnted, order: 0 }
-  })
-  buttons.push({
-    url: "/",
-    txt: "Home",
-    order: 1
-  })
+  const buttons = [
+    {
+      url: "/",
+      txt: "Home",
+      order: 1
+    },
+    {
+      url: "/page/product",
+      txt: "Products",
+      order: 0
+    },
+    {
+      url: "/page/integration",
+      txt: "Integrations",
+      order: 0
+    },
+  ]
 
   return res.status(200).send(buttons.sort((a, b) => b.order - a.order)).sort();
 }
