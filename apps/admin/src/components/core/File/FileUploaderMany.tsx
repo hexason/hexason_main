49f1148eea097eed4d4108b0ce97d552bf6fed87
Cybinon <nikorunikk@gmail.com>
2023-04-19 16:42:56 +0800

    Image upload done

--- apps/admin/src/components/core/File/FileUploaderMany.tsx
+++ apps/admin/src/components/core/File/FileUploaderMany.tsx
@@ -39,7 +39,8 @@ export default function FileUploaderMany({ imgs, onChange }: { imgs?: ImageUploa
       });
   };
 
-  const runner = () => {
+  const handleUploader = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
     if (loading) return;
     setLoading(true);
     const logger = async (img: ImageUploadManyType) => {
@@ -49,7 +50,11 @@ export default function FileUploaderMany({ imgs, onChange }: { imgs?: ImageUploa
       img.isUploaded = true;
       return img;
     }
-    const files = new Queue<ImageUploadManyType, ImageUploadManyType[]>(images.filter(m => !m.isUploaded), logger)
+    let preImages = []
+    for (let i = 0; i < e.target.files.length; i++) {
+      preImages.push({ name: e.target.files[i].name, url: "", isUploaded: false, file: e.target.files[i] });
+    }
+    const files = new Queue<ImageUploadManyType, ImageUploadManyType[]>(preImages.filter(m => !m.isUploaded), logger);
     const listener = files.start();
     listener.on("progress", ({ data }) => {
       if (!data) return;
@@ -60,17 +65,7 @@ export default function FileUploaderMany({ imgs, onChange }: { imgs?: ImageUploa
     })
     listener.on("end", () => {
       setLoading(false);
-    })
-  }
-
-  const handleUploader = (e: ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return;
-    for (let i = 0; i < e.target.files.length; i++) {
-      setImages((prev) => {
-        if (!e.target.files) return prev;
-        return [...prev, { name: e.target.files[i].name, url: "", isUploaded: false, file: e.target.files[i] }]
-      });
-    }
+    });
   }
 
   const removeImage = (name: string) => {
@@ -78,42 +73,38 @@ export default function FileUploaderMany({ imgs, onChange }: { imgs?: ImageUploa
       const newImages = prev.filter(is => is.name !== name);
       return newImages;
     })
-
   }
 
   return (
-    <>
-      {images.find(i => !i.isUploaded) ? <Button onClick={runner} colorScheme="black" isLoading={loading}>Upload</Button> : null}
-      <Wrap>
-        {images.map(e => <Box
-          key={e.url}
-          margin="0"
-          w="75px"
-          position={"relative"}
-          className="hover-overlay-show"
-        >
-          <Image w="75px" src={e.url} alt={e.name} />
-          <Stack className="overlay-image" top={0} position={"absolute"} w="100%" h="100%" bg="#0000007B">
-            <Button onClick={() => removeImage(e.name)} colorScheme="red"><TrashIcon height={20} fill={"white"} /></Button>
-          </Stack>
-        </Box>)}
-        <Button colorScheme="blackAlpha" position={"relative"}>
-          +
-          <Input
-            type="file"
-            height="100%"
-            width="100%"
-            position="absolute"
-            top="0"
-            left="0"
-            opacity="0"
-            aria-hidden="true"
-            accept="image/*"
-            multiple
-            onChange={handleUploader} />
-        </Button>
-
-      </Wrap>
-    </>
+    <Wrap>
+      {images.map(e => <Box
+        key={e.url}
+        margin="0"
+        w="75px"
+        position={"relative"}
+        className="hover-overlay-show"
+      >
+        <Image w="75px" src={e.url} alt={e.name} />
+        <Stack className="overlay-image" top={0} position={"absolute"} w="100%" h="100%" bg="#0000007B">
+          <Button onClick={() => removeImage(e.name)} colorScheme="red"><TrashIcon height={20} fill={"white"} /></Button>
+        </Stack>
+      </Box>)}
+      <Button isLoading={loading} colorScheme="blackAlpha" position={"relative"}>
+        +
+        <Input
+          type="file"
+          height="100%"
+          width="100%"
+          position="absolute"
+          top="0"
+          left="0"
+          opacity="0"
+          aria-hidden="true"
+          accept="image/*"
+          multiple
+          disabled={loading}
+          onChange={handleUploader} />
+      </Button>
+    </Wrap>
   )
 }
