e7dfc3e7237e7baf08da4a680b5eac5db43ced9a
Cybinon <nikorunikk@gmail.com>
2023-04-21 11:26:31 +0800

    detail engine added

--- apps/client/src/pages/shop/index.tsx
+++ apps/client/src/pages/shop/index.tsx
@@ -1,9 +1,7 @@
 import ProductCard from "@/components/cards/ProductCard";
-import { Box, Container, Grid } from "@chakra-ui/react";
+import { Container, Grid } from "@chakra-ui/react";
 
 export default function ShoppPage() {
-
-
   return (
     <Container maxW="container.xl" p={0}>
       <Grid templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)",  "repeat(5, 1fr)"]} gap={6}>
