2ba057cf5ac88691904937bc52510bfe364e270b
Cybinon <nikorunikk@gmail.com>
2023-05-18 13:38:20 +0800

    enh: added @sales24 package

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import ChatBox from "@/components/core/ChatBox"
 import DefaultLayout from "@/components/layout/DefaultLayout"
+import { FileUploader, FileUploaderMany } from "@sales24/client";
 
 export default function Home() {
   return (
     <DefaultLayout>
       <ChatBox />
+      <FileUploader />
+      <FileUploaderMany />
     </DefaultLayout>
   )
 }
