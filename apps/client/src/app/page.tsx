c73638856ed38b154751a8d6c7774e19f3eae41e
Cybinon <nikorunikk@gmail.com>
2023-05-18 13:43:55 +0800

    upload manager done

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client"
 import ChatBox from "@/components/core/ChatBox"
 import DefaultLayout from "@/components/layout/DefaultLayout"
+import { Box, Heading } from "@chakra-ui/react";
 import { FileUploader, FileUploaderMany } from "@sales24/client";
 
 export default function Home() {
   return (
     <DefaultLayout>
       <ChatBox />
-      <FileUploader />
-      <FileUploaderMany />
+      <Heading>One File Upload</Heading>
+      <Box color="white" h="400px" w="200px">
+        <FileUploader />
+      </Box>
+
+      <Heading>Simple Upload Manager</Heading>
+      <Box h="400px" w="100%">
+        <FileUploaderMany />
+      </Box>
     </DefaultLayout>
   )
 }
