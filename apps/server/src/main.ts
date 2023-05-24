084a47654bd071933137f983d783edaae4d5feac
Cybinon <nikorunikk@gmail.com>
2023-05-24 19:37:13 +0800

    cors accept

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,7 +6,7 @@ import { IoCustomAdapter } from './utils/io-adapter';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { cors: true });
 
   app.useWebSocketAdapter(new IoCustomAdapter(app));
   app.useGlobalPipes(new ValidationPipe());
