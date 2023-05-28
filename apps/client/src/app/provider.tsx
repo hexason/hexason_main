4f821c7ba28e87bcf478d468fd20db5434cb2f74
Khebe <khebe@infinite.mn>
2023-05-28 15:13:52 +0800

    theming section added, @next/font removed

--- apps/client/src/app/provider.tsx
+++ apps/client/src/app/provider.tsx
@@ -1,37 +1,20 @@
 // app/providers.tsx
-'use client'
-import { AuthContextProvider } from '@/context/AuthContext'
-import apollo from '@/lib/Apollo'
-import { ApolloProvider } from '@apollo/client'
-import { CacheProvider } from '@chakra-ui/next-js'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+"use client";
+import { AuthContextProvider } from "@/context/AuthContext";
+import apollo from "@/lib/Apollo";
+import theme from "@/theme";
+import { ApolloProvider } from "@apollo/client";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export function Providers({
-  children
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <CacheProvider>
-      <ChakraProvider theme={extendTheme({
-        config: {
-          initialColorMode: 'light',
-          useSystemColorMode: false,
-        },
-        styles: {
-          global: {
-            body: {
-              bg: "#D2d2d4"
-            }
-          }
-        }
-      })}>
-        <AuthContextProvider>
-          <ApolloProvider client={apollo}>
-            {children}
-          </ApolloProvider>
-        </AuthContextProvider>
-      </ChakraProvider>
-    </CacheProvider>
-  )
+export function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<CacheProvider>
+			<ChakraProvider theme={theme}>
+				<AuthContextProvider>
+					<ApolloProvider client={apollo}>{children}</ApolloProvider>
+				</AuthContextProvider>
+			</ChakraProvider>
+		</CacheProvider>
+	);
 }
--- apps/client/src/components/core/header/BasketWithFavButton.tsx
+++ apps/client/src/components/core/header/BasketWithFavButton.tsx
@@ -1,17 +1,35 @@
-import { ShopCartIcon, HeartIcon } from "@/assets/icons"
-import { HStack, Button, Badge } from "@chakra-ui/react"
+import { ShopCartIcon, HeartIcon } from "@/assets/icons";
+import { HStack, Button, Badge } from "@chakra-ui/react";
 
 export const BasketWithFavButton = () => {
-  return (
-    <HStack h="100%" justifyContent={"end"} alignItems={"center"} spacing={"0"}>
-      <Button position={"relative"} h="80%" p={6} border="1px solid green" borderLeftRadius={"full"} colorScheme="green">
-        <ShopCartIcon fill="white" width={30} height={30} />
-        <Badge position={"absolute"} top="3" left="3" borderRadius={"full"}>2</Badge>
-      </Button>
-      <Button position={"relative"} h="80%" p={6} border="1px solid green" borderRightRadius={"full"} colorScheme="green">
-        <HeartIcon fill="white" width={30} height={30} />
-        <Badge position={"absolute"} top="3" left="3" borderRadius={"full"}>2</Badge>
-      </Button>
-    </HStack>
-  )
-}
+	return (
+		<HStack h="100%" justifyContent={"end"} alignItems={"center"} spacing={"0"}>
+			<Button
+				position={"relative"}
+				h="80%"
+				p={6}
+				border="1px solid green"
+				borderLeftRadius={"full"}
+				colorScheme="green"
+			>
+				<ShopCartIcon fill="white" width={30} height={30} />
+				<Badge position={"absolute"} top="3" left="3" borderRadius={"full"}>
+					2
+				</Badge>
+			</Button>
+			<Button
+				position={"relative"}
+				h="80%"
+				p={6}
+				border="1px solid green"
+				borderRightRadius={"full"}
+				colorScheme="green"
+			>
+				<HeartIcon fill="white" width={30} height={30} />
+				<Badge position={"absolute"} top="3" left="3" borderRadius={"full"}>
+					2
+				</Badge>
+			</Button>
+		</HStack>
+	);
+};
