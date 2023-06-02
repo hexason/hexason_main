842e9865748bb212798c3b789f4ff4a0db9fd80c
Cybinon <nikorunikk@gmail.com>
2023-06-02 15:24:22 +0800

    feat: Redis added

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -1,10 +1,21 @@
+import type { RedisClientOptions } from 'redis';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseConnectionModule, TypeOrmConnectionModule } from './connection';
+import { redisStore } from 'cache-manager-redis-store';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
 
 export const CoreModule = [
   MongooseConnectionModule,
   TypeOrmConnectionModule,
+  CacheModule.registerAsync<RedisClientOptions>({
+    useFactory: async () => ({
+      store: (await redisStore({
+        url: process.env.REDIS_URL,
+      })) as unknown as CacheStore,
+    }),
+    isGlobal: true,
+  }),
   ThrottlerModule.forRoot({
     ttl: 60,
     limit: 100,
