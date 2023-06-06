0d31515c0207430e3a7e7b5a3cbe36dcc8a09fa9
Cybinon <nikorunikk@gmail.com>
2023-06-06 22:04:15 +0800

    fix: Duplicated Layout remove

--- apps/client/src/app/shop/page.tsx
+++ apps/client/src/app/shop/page.tsx
@@ -1,10 +1,7 @@
 import { ProductList } from "@/components/build";
-import { GeneralLayout } from "@/components/layout";
 
 export default function Page() {
   return (
-    <GeneralLayout>
-      <ProductList />
-    </GeneralLayout>
+    <ProductList />
   )
 }
