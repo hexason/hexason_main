676afbbe93805474a5b1e8f5d9b0d464d11f3712
Cybinon <nikorunikk@gmail.com>
2023-06-06 21:03:33 +0800

    Feat: customer with GQL access checker guard

--- apps/server/src/modules/auth/auth.decorator.ts
+++ apps/server/src/modules/auth/auth.decorator.ts
@@ -1,6 +1,8 @@
 import { AdminJWTGuard } from './admin.guard';
-import { UseGuards, applyDecorators } from '@nestjs/common';
+import { ExecutionContext, UseGuards, applyDecorators, createParamDecorator } from '@nestjs/common';
 import { ApiBearerAuth, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { CustomerJWTGuard } from './customer.guard';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 export function Auth() {
   return applyDecorators(
@@ -9,3 +11,16 @@ export function Auth() {
     ApiUnauthorizedResponse({ description: 'Unauthorized' }),
   );
 }
+
+export function CustomerAuth() {
+  return applyDecorators(
+    UseGuards(CustomerJWTGuard),
+    ApiBearerAuth('access-token'),
+    ApiUnauthorizedResponse({ description: 'Unauthorized' }),
+  );
+}
+
+export const CurrentUserGQL = createParamDecorator((data: unknown, context: ExecutionContext) => {
+  const ctx = GqlExecutionContext.create(context);
+  return ctx.getContext().req.user;
+});
