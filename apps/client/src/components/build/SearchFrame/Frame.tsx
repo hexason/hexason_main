bb8e3d0eac8f0cea6b3f61c4c6ca79569bf6ac42
Khebe <khebe@infinite.mn>
2023-06-30 20:03:57 +0800

    fix: hot fix on frame

--- apps/client/src/components/build/SearchFrame/Frame.tsx
+++ apps/client/src/components/build/SearchFrame/Frame.tsx
@@ -8,70 +8,78 @@ import React, { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 export const Frame = ({
-  query,
-  provider,
-  limit,
-  infinite
+	query,
+	provider,
+	limit,
+	infinite,
 }: {
-  query: string;
-  provider: string;
-  limit?: number
-  infinite?: boolean
+	query: string;
+	provider: string;
+	limit?: number;
+	infinite?: boolean;
 }) => {
-  const [page, setPage] = useState(0);
-  const [products, setProducts] = useState<any>([]);
-  const { loading, data, refetch } = useQuery(searchProducts, {
-    variables: {
-      data: {
-        page: page || 0,
-        limit: limit || 5,
-        provider,
-        query,
-      },
-    },
-  });
-  useEffect(() => {
-    if (data?.searchProducts.items)
-      setProducts((prev: any) => [...prev, ...data?.searchProducts.items]);
-  }, [data]);
+	const [page, setPage] = useState(0);
+	const [products, setProducts] = useState<any>([]);
+	const { loading, data, refetch } = useQuery(searchProducts, {
+		variables: {
+			data: {
+				page: page,
+				limit: limit || 5,
+				provider,
+				query,
+			},
+		},
+	});
+	useEffect(() => {
+		console.log("data===>", data);
+		if (data?.searchProducts.items)
+			if (limit) {
+				setProducts((prev: any) => [...prev, ...data?.searchProducts.items]);
+			} else {
+				setProducts(data?.searchProducts.items);
+			}
+	}, [data]);
 
-  const loadMoreItems = async () => {
-    if (loading) return;
-    setPage(prev => prev + 1);
-    refetch()
-  }
+	const loadMoreItems = async () => {
+		if (!limit) return;
+		if (loading) return;
+		setPage((prev) => prev + 1);
+		refetch();
+	};
 
-  if (!infinite) {
-    if (loading) return <ThreeDotsWave />
-    return (
-      <>
-        <Grid templateColumns={"repeat(5, 1fr)"}>
-          {products.map((product: any) => (
-            <ProductCard key={product.id + Date.now()} product={product} />
-          ))}
-        </Grid>
-        {products.length === 0 && <>Бараа олдсонгүй</>}
-      </>
-    )
-  }
-  return (
-    <Box minH={"100vh"}>
-      <InfiniteScroll
-        dataLength={products.length}
-        next={loadMoreItems}
-        refreshFunction={loadMoreItems}
-        pullDownToRefresh
-        pullDownToRefreshThreshold={50}
-        hasMore={true}
-        loader={<ThreeDotsWave />}
-      >
-        <Grid templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(5, 1fr)" }}>
-          {products.map((product: any) => (
-            <ProductCard key={product.id + Date.now()} product={product} />
-          ))}
-        </Grid>
-        {products.length === 0 && <>Бараа олдсонгүй</>}
-      </InfiniteScroll>
-    </Box>
-  );
+	if (!infinite) {
+		if (loading) return <ThreeDotsWave />;
+		return (
+			<>
+				<Grid templateColumns={"repeat(5, 1fr)"}>
+					{products.map((product: any) => (
+						<ProductCard key={product.id + Date.now()} product={product} />
+					))}
+				</Grid>
+				{products.length === 0 && <>Бараа олдсонгүй</>}
+			</>
+		);
+	}
+	return (
+		<Box minH={"100vh"}>
+			<InfiniteScroll
+				dataLength={products.length}
+				next={loadMoreItems}
+				refreshFunction={loadMoreItems}
+				pullDownToRefresh
+				pullDownToRefreshThreshold={100}
+				hasMore={limit ? true : false}
+				loader={<ThreeDotsWave />}
+			>
+				<Grid
+					templateColumns={{ base: "repeat(2, 1fr)", md: "repeat(5, 1fr)" }}
+				>
+					{products.map((product: any) => (
+						<ProductCard key={product.id + Date.now()} product={product} />
+					))}
+				</Grid>
+				{products.length === 0 && <>Бараа олдсонгүй</>}
+			</InfiniteScroll>
+		</Box>
+	);
 };
